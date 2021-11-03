6420df2b011179889963b6be065fe7e3366bc094
AmnaSZafar <amna.s.zafar@gmail.com>
2021-11-03 12:57:32 +0500

    1

---
+++ config/constants.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'jazzcash' => [
+        'MERCHANT_ID' 	 => 'MC28910',
+        'PASSWORD' 		 => '',
+		'INTEGERITY_SALT'=> '',
+		'CURRENCY_CODE'  => 'PKR',
+		'VERSION'		 => '1.1',
+		'LANGUAGE'  	 => 'EN',
+		
+		
+		'RETURN_URL'  => 'http://127.0.0.1:8000/paymentStatus',
+		'TRANSACTION_POST_URL'  => 'https://sandbox.jazzcash.com.pk/CustomerPortal/transactionmanagement/merchantform/'
+
+
+		
+    ]
+];
